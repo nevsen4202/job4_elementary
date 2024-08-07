856dfa6850a11f2af0fdb33a5dac403d9a3bd00f
nevsen4202 <nevsen2024@yandex.ru>
2024-08-07 21:09:08 +0300

    Разбор первой программы по "взрослому" [#505151]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Evgeniy Syropaytov");
+        System.out.println("07.08.2024");
+    }
+}
