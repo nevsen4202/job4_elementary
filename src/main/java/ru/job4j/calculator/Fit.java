33217e388edbdd9baf3c39126f23e92dbbbcbfd9
nevsen4202 <nevsen2024@yandex.ru>
2024-08-13 00:47:59 +0300

    2. Тесты для задачи "идеальный вес" [#505260 #508494]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -3,12 +3,10 @@ package ru.job4j.calculator;
 public class Fit {
 
     public static double manWeight(short manheight) {
-        // Formula for man's weight: (height in cm - 100) * 1.15
         return (manheight - 100) * 1.15;
     }
 
     public static double womanWeight(short womanheight) {
-        // Formula for woman's weight: (height in cm - 110) * 1.15
         return (womanheight - 110) * 1.15;
     }
 
