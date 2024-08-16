ca045bd9112bbc44505e6fec966e0d19727ec4cc
nevsen4202 <nevsen2024@yandex.ru>
2024-08-16 21:29:43 +0300

    1. Модульные тесты [#505277 #508299]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -2,10 +2,10 @@ package ru.job4j;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-               return value / 90;
+        return value / 90;
     }
 
     public static float rubleToDollar(float value) {
-                return value / 75;
+        return value / 75;
     }
 }
