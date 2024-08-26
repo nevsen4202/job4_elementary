4e597b678aa29154156c67c7f680bad9727cac12
nevsen4202 <nevsen2024@yandex.ru>
2024-08-26 22:17:38 +0300

    Классический поиск перебором [#505227]

---
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int element) {
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == element) {
+                return index;
+            }
+        }
+        return -1;
+    }
+}
+
+
