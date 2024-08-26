56355910d8cb483b8c6639a8ee1eb942cc10db8c
nevsen4202 <nevsen2024@yandex.ru>
2024-08-26 23:00:19 +0300

    Выход за границу массива [#505229]

---
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class SwitchArray {
+    public static int[] swapBorder(int[] array) {
+        if (array.length >= 2) {
+        int temp = array[0];
+        array[0] = array[array.length - 1];
+        array[array.length - 1] = temp;
+    }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[] {1, 2, 3, 4, 5, 6};
+        int[] result = swapBorder(numbers);
+        for (int index = 0; index < result.length; index++) {
+            System.out.print(result[index]);
+        }
+    }
+}
+
