0b9d7bf8602381d481661cc488fcecaf7db0d60b
nevsen4202 <nevsen2024@yandex.ru>
2024-09-13 19:48:50 +0300

    6.7.4. Выигрышные комбинации в сокобан [#505248]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -89,9 +89,9 @@ public class MatrixCheckTest {
             assertThat(result).containsExactly(expected);
         }
 
-    @Test
-    public void whenDataMonoByTrueThenTrue() {
-        char[][] input = {
+        @Test
+        public void whenDataMonoByTrueThenTrue() {
+            char[][] input = {
                 {' ', ' ', 'X', ' ', ' '},
                 {' ', ' ', 'X', ' ', ' '},
                 {' ', ' ', 'X', ' ', ' '},
@@ -100,45 +100,45 @@ public class MatrixCheckTest {
         };
         boolean result = MatrixCheck.isWin(input);
         assertThat(result).isTrue();
-    }
+        }
 
-    @Test
-    public void whenDataNotMonoByTrueThenFalse() {
-        char[][] input = {
+        @Test
+        public void whenDataNotMonoByTrueThenFalse() {
+            char[][] input = {
                 {' ', ' ', 'X', ' ', ' '},
                 {' ', ' ', 'X', ' ', ' '},
                 {' ', 'X', ' ', ' ', ' '},
                 {' ', ' ', 'X', ' ', ' '},
                 {' ', ' ', 'X', ' ', ' '},
-        };
-        boolean result = MatrixCheck.isWin(input);
-        assertThat(result).isFalse();
-    }
+            };
+            boolean result = MatrixCheck.isWin(input);
+            assertThat(result).isFalse();
+        }
 
-    @Test
-    public void whenDataHMonoByTrueThenTrue() {
-        char[][] input = {
+        @Test
+        public void whenDataHMonoByTrueThenTrue() {
+            char[][] input = {
                 {' ', ' ', ' ', ' ', ' '},
                 {' ', ' ', ' ', ' ', ' '},
                 {'X', 'X', 'X', 'X', 'X'},
                 {' ', ' ', 'X', ' ', ' '},
                 {' ', ' ', 'X', ' ', ' '},
-        };
-        boolean result = MatrixCheck.isWin(input);
-        assertThat(result).isTrue();
-    }
+            };
+            boolean result = MatrixCheck.isWin(input);
+            assertThat(result).isTrue();
+        }
 
-    @Test
-    public void whenDataDiagMonoIsTrueThenFalse() {
-        char[][] input = {
+            @Test
+            public void whenDataDiagMonoIsTrueThenFalse() {
+                char[][] input = {
                 {'X', ' ', ' ', ' ', ' '},
                 {' ', 'X', ' ', ' ', ' '},
                 {' ', ' ', 'X', ' ', ' '},
                 {' ', ' ', ' ', 'X', ' '},
                 {' ', ' ', ' ', ' ', 'X'},
-        };
-        boolean result = MatrixCheck.isWin(input);
-        assertThat(result).isFalse();
-    }
+            };
+            boolean result = MatrixCheck.isWin(input);
+            assertThat(result).isFalse();
+        }
 }
 
