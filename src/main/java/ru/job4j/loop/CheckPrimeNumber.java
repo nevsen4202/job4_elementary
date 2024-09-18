a531c0571fac3e010e0a0b1b099a3a001c5cbdb3
nevsen4202 <nevsen2024@yandex.ru>
2024-09-18 17:34:46 +0300

    Простое число (оператор прерывания break) [#505214 #510457]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,14 +2,11 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        if (number < 2) {
-            return false;
-        }
         for (int n = 2; n < number; n++) {
             if (number % n == 0) {
                 return false;
             }
         }
-        return true;
+        return number > 1;
     }
 }
